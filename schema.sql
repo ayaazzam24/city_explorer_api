e40a186c8d275562e4b532cbf10872ad539dc4d3
ayaazzam24 <ayaazzam0101@gmail.com>
2021-04-01 00:04:39 +0300

    solveeeeeeeeeeeeeeeeeeeeeeeeeeee it

--- schema.sql
+++ schema.sql
@@ -3,7 +3,7 @@ CREATE TABLE IF NOT EXISTS
 location(
     id SERIAL NOT NULL,
     search_query VARCHAR(256),
-    formatted_query VARCHAR(256),
-    latitude  NUMERIC(10, 7),
-    longitude NUMERIC(10, 7)
+    formatted_query VARCHAR(255),
+    latitude  VARCHAR(255),
+    longitude VARCHAR(255)
 );
